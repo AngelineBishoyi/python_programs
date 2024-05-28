87a7cb63c4740741de4320d11bf1d808fc1d3c08
Angeline Bishoyi <abishoyi@msa.miraclesoft.com>
2024-05-29 00:41:44 +0530

    commit

---
+++ inh.py
@@ -0,0 +1,20 @@
+class person(object):
+    def __init__(amy,n,a):
+        amy.name=n
+        amy.age=a
+    def display(sha):
+        print(sha.name,sha.age)
+emp=person('Angeline',22)
+emp.display()
+class Emp(person):
+    def print(shat):
+        print("Employee is added")
+a=Emp('Roseline',24)
+a.print()
+a.display()
+
+
+
+class z:
+    print('AAAAA')
+obj=z()
